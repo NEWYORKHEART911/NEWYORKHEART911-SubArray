931f82e4eab94b4885877fb9646ea3844d149bfc
NEWYORKHEART911 <lavillelumiere8@gmail.com>
2023-02-21 05:36:30 +0000

    changes1

--- src/main/java/SubArray.java
+++ src/main/java/SubArray.java
@@ -4,7 +4,7 @@ public class SubArray {
      * Return only a part of an array. For instance, given the array {1,2,3,4,5}, a sub array with start 1 and end 4
      * would result in {2,3,4}, because the starting index is inclusive, but the ending index is exclusive.
      *
-     * You will need to create a new array whose size is only the amound of elements requested between the start and
+     * You will need to create a new array whose size is only the amount of elements requested between the start and
      * end index, and then write a for loop which copies values from nums over to this new resulting array.
      *
      * @param nums an array of ints.
@@ -13,6 +13,10 @@ public class SubArray {
      * @return a sub-array of nums containing the values between start and end.
      */
     public int[] sub(int[] nums, int start, int end){
-        return null;
+        int[] slice = new int[end-start];
+        for(int j=0; j<end-start; j++) {
+            slice[j] = nums[start+j];
+        }
+        return slice;
     }
 }
